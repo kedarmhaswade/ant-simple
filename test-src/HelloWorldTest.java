242d0cb85de7c50c15419060c4c608c090a574c6
Kedar Mhaswade <kedar.mhaswade@gmail.com>
2010-08-18 07:15:08 -0700

    initial commit

---
+++ test-src/HelloWorldTest.java
@@ -0,0 +1,11 @@
+public class HelloWorldTest extends junit.framework.TestCase {
+
+    @Test
+    public void test() {
+      System.out.println("Succeeds!")
+    }
+    
+    /*public void testWillAlwaysFail() {
+        fail("An error message");
+    }*/
+}
